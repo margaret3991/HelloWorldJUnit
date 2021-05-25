a5a87db0315d94f0ed55f5302253afbe52f01d6f
margaret3991 <margaret.lanphere@gmail.com>
2021-05-25 12:11:05 -0700

    Updating JUnit test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,7 @@
 
 
 class HelloWorld {
-	public static String hello = "Hello, World!";
+	//public static String hello = "Hello, World!";
 
     public static void main(String[] args) {
     	String hello = "Hello";
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,18 +1,44 @@
 import org.junit.Assert;
 import org.junit.Test;
-
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.After;
 
 import java.io.*;
 import java.util.Locale;
 
 public class HelloWorldTest {
 
+	private final PrintStream standardOut = System.out;
+	private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+	
+	@Before
+	public void setUp() {
+    	System.setOut(new PrintStream(outputStreamCaptor));
+	}
+	
 	@Test
+	public void givenSystemOutRedirection_whenInvokePrintln_thenOutputCaptorSuccess() {
+    	HelloWorld.main(new String[] {"arg1", "arg2", "arg3"});
+        
+    	Assert.assertEquals("Hello", outputStreamCaptor.toString()
+      	.trim());
+	}
+	
+/*
+	@Test
+	@Ignore
 	public void HelloTest() {
 		HelloWorld.main(new String[] {"arg1", "arg2", "arg3"});
 		//Assert.assertEquals("Hello", HelloWorld.hello);
 		//System.out.println(HelloWorld.hello);
 		Assert.assertEquals("Hello, World!", HelloWorld.hello);
 	
+	}
+*/
+	
+	@After
+	public void tearDown() {
+    	System.setOut(standardOut);
 	}
 }
