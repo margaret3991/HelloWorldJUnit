af2a2dd1c62fb3826bc20cebb0bedba70ef31b1d
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-06-01 15:33:19 -0700

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,6 +5,32 @@
 class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World! ") //an extra space was added in the end to make test case failed
+    }
+    
+    public static void printStarPiramid() {
+        System.out.println("    * "); //an extra space was added in the end to make test case failed
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+    }
+    
+    public static void printFace() {
+        System.out.println(" +\"\"\"\"\"+  "); //an extra space was added in the end to make test case failed
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+    
+    public static void printLongSentence() 
+    {
+        System.out.println("This is a very long sentence that contains over 100 characters, and it should cause a warming in checkstyle!");
+    }
+    
+    public static void nothing(){
+        if (true) if (false) 
+            for (int i=0; i<10; i++) i++;
     }
 }
